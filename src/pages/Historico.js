82ea0e7b4bf6b9af1063d91240138c5328a83c53
vitorlc <vitor_cirqueira1@hotmail.com>
2020-10-08 19:18:23 -0300

    Add Historico Page

--- src/pages/Historico.js
+++ src/pages/Historico.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { View, SafeAreaView } from 'react-native';
 import {
   Icon,
-  Button,
   Header,
-  Text,
-  CheckBox
+  ListItem,
+  Text
 } from 'react-native-elements'
 import Padrao from '../styles/default'
+import db from '../services/db'
+import moment from 'moment'
 
 const Historico = ({navigation}) => {
+  const [historico, setHistorico] = useState([])
+    
+  useEffect(async () => {
+    ;(async () => {
+      let historicoFetch = await (await db.getSensors()).map(race => ({
+        fuel: race.fuel,
+        race_start: moment(race.race_start.toDate()).format("DD/MM/YYYY HH:mm:ss"),
+        race_end: moment(race.race_end.toDate()).format("DD/MM/YYYY HH:mm:ss")
+      }))
+      setHistorico(historicoFetch)
+    })()
+  }, [])
+  
   return (
     <SafeAreaView style={{flex: 1}}>
       <Header
@@ -19,11 +33,26 @@ const Historico = ({navigation}) => {
           <Icon name='menu' color='#fff' onPress={()=> navigation.openDrawer()}/>
         }
         containerStyle={{
-          height: 60,
           backgroundColor: '#78bc6d',
-          paddingTop: 1
         }}
       />
+      <View>
+      {
+        historico.length > 0 ? 
+          historico.map((l, i) => (
+            <ListItem key={i} bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title style={{ color: 'black', fontWeight: 'bold' }}>#{i+1} Corrida</ListItem.Title>
+                <ListItem.Subtitle style={{ color: 'black' }}>Início: {l.race_start} </ListItem.Subtitle>
+                <ListItem.Subtitle style={{ color: 'black' }}>Fim: {l.race_end}</ListItem.Subtitle>
+              </ListItem.Content>
+              <ListItem.Chevron color="black" />
+            </ListItem>     
+          ))
+        :
+        <Text h4 style={{textAlign: 'center', alignItems: 'center'}}> Sem Corridas </Text>
+      }
+      </View>
     </SafeAreaView>
   )
 }
