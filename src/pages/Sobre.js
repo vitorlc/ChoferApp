360a85d50f61a6d4645e354ae56a3f668ef38e62
vitorlc <vitor_cirqueira1@hotmail.com>
2020-10-08 20:12:13 -0300

    Add Sobre page

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import {
   Icon,
-  Button,
+  Divider,
   Header,
   Text,
-  CheckBox
 } from 'react-native-elements'
 import Padrao from '../styles/default'
 
@@ -19,11 +18,24 @@ const Sobre = ({navigation}) => {
           <Icon name='menu' color='#fff' onPress={()=> navigation.openDrawer()}/>
         }
         containerStyle={{
-          height: 60,
           backgroundColor: '#78bc6d',
-          paddingTop: 1
         }}
       />
+      <View style= {{flex: 1, margin: 10}}>
+        <Text style={{fontSize: 24}}>Essa aplicação foi desenvolvida como Projeto de Final de Curso (PFC) do curso de Sistemas de Informação da Universidade Federal de Goiás (UFG).</Text>
+        <Divider style={{marginTop: 10}}/>
+
+        <Text style={{fontSize: 18, marginTop: 10}}>Aluno: Vitor de Lima Cirqueira</Text>
+        <Text style={{fontSize: 18}}>Orientador: Ricardo Augusto Pereira Franco</Text>
+        <Divider style={{marginTop: 10}}/>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{fontSize: 18, marginTop: 10 ,alignItems: 'center'}}>Link para o repositório no Github: </Text>
+        </View>
+        <View style={{flex:1, flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
+          <Text style={{fontSize: 18, color:'#0077ff'}}>ChoferApp </Text>
+          <Icon name='github' type='font-awesome' color='#0077ff' />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
