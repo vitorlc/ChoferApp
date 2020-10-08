55684714d27ea1002da1369db5d4dd5f837af7c5
vitorlc <vitor_cirqueira1@hotmail.com>
2020-10-08 20:11:56 -0300

    Change Theme color of Navigation Drawer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler'
 import React from 'react';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'  
 
 import rootReducer from './store'
@@ -10,12 +10,20 @@ import Main from './pages/Main'
 import Sobre from './pages/Sobre'
 import Historico from './pages/Historico'
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(120, 188, 109)',
+  },
+};
+
 const store = createStore(rootReducer)
 const Drawer = createDrawerNavigator();
 
 const App = () => (
   <Provider store={store}>
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Drawer.Navigator>
         <Drawer.Screen options={{headerShown: false}} name="Home" component={Main} />
         <Drawer.Screen options={{headerShown: false}} name="Histórico" component={Historico} />
