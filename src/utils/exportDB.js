3e809bd803580aa474bda6b9ca9d411683ef3449
vitorlc <vitor_cirqueira1@hotmail.com>
2020-10-14 21:19:36 -0300

    Improve export script, add consumption and args for more than 1 document

--- src/utils/exportDB.js
+++ src/utils/exportDB.js
@@ -5,7 +5,7 @@ const Excel = require('exceljs')
 
 var serviceAccount = require("./chofer-app-f5510-firebase-adminsdk-skx0a-f9dac2d8d4.json")
 const appName = '[DEFAULT]'
-
+const myArgs = process.argv.slice(2);
 ;(async () => {
   firestoreService.initializeApp(serviceAccount, 'chofer-app-f5510', appName);
   
@@ -15,12 +15,14 @@ const appName = '[DEFAULT]'
 
   fs.writeFileSync('DB_DATA.json', JSON.stringify(collections))
   raceObject = collections 
-  console.log("raceObject", raceObject)
+  // console.log("raceObject", raceObject)
 
   const workbook = new Excel.Workbook();
   let worksheet1 = workbook.addWorksheet("Speed")
   let worksheet2 = workbook.addWorksheet("Rpm")
   let worksheet3 = workbook.addWorksheet("MAF")
+  let worksheet4 = workbook.addWorksheet("Consumption")
+
   worksheet1.columns = [
     {header: 'Date', key: 'date', width: 40},
     {header: 'Value', key: 'value', width: 40},
@@ -33,22 +35,32 @@ const appName = '[DEFAULT]'
     {header: 'Date', key: 'date', width: 40},
     {header: 'Value', key: 'value', width: 40},
   ]
-    
-  for (let id of Object.keys(raceObject.races)) {
-    raceObject.races[id].speed_data.forEach(data => {
-      data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
-      worksheet1.addRow(data)
-    })
-    raceObject.races[id].rpm_data.forEach(data => {
-      data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
-      worksheet2.addRow(data)
-    })
-    raceObject.races[id].maf_data.forEach(data => {
-      data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
-      worksheet3.addRow(data)
-    })
-    break // SOMENTE A PRIMEIRA CORRIDA POR ENQUANTO
-  }
+  worksheet4.columns = [
+    {header: 'Date', key: 'date', width: 40},
+    {header: 'Value', key: 'value', width: 40},
+  ]
+
+  let raceFiltered = Object.values(raceObject.races)
+    .filter(race => moment(race.race_start.toDate()).format("DD/MM/YYYY") == moment(myArgs, 'DD/MM/YYYY').format("DD/MM/YYYY"))
+    .sort((a,b) => new Date(b.race_start.toDate()) -  new Date(a.race_start.toDate()))[0]
+  
+  raceFiltered.speed_data.forEach(data => {
+    data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
+    worksheet1.addRow(data)
+  })
+  raceFiltered.rpm_data.forEach(data => {
+    data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
+    worksheet2.addRow(data)
+  })
+  raceFiltered.maf_data.forEach(data => {
+    data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
+    worksheet3.addRow(data)
+  })
+  raceFiltered.consume_data.forEach(data => {
+    data.date = moment(data.date.toDate()).format("DD/MM/YYYY HH:mm:ss")
+    worksheet4.addRow(data)
+  })
+  
   workbook.xlsx.writeFile(`./EXPORT-DB-${moment().format('DD-MM-YYYY')}.xls`).then(() => {
     console.log("\n=============================================================")
     console.log('PLANILHA CRIADA')
